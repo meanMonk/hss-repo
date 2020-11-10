c728f14b764da06cf8039409d1b1933b37bfe1f5
sahil kashetwar <kashetwartechlab@gmail.com>
2020-11-10 23:32:36 +0530

    integration gatsby with wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,11 +20,35 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from WordPress.
+     */
+    // highlight-start
+    {
+      resolve: `gatsby-source-wordpress-experimental`,
+      options: {
+        /*
+         * The full URL of the WordPress site's GraphQL API.
+         * Example : 'https://www.example-site.com/graphql'
+         */
+        hostingWPCOM: false,
+        url: `http://mes-society.gigassociates.com/graphql`,
+        auth: {
+          htaccess_user: "admin",
+          htaccess_pass: "mes_society@123",
+          htaccess_sendImmediately: false,
+        },
+        includedRoutes: ["/posts", "/pages", "/categories", "/tags", "/users"],
       },
     },
   ],
